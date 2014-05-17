4db85b9c76295de623ced4aa30541243a1b4e3f8
kradnozd <kradnozd@gmail.com>
2014-05-17 11:24:23 +0700

    v1.3

--- js/bkgm.js
+++ js/bkgm.js
@@ -36,4 +36,20 @@ $(document).ready(function(){
     		}
     	}
     );
+
+    $.easing.elasout = function(x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	};
+    $("#section-link a").click( function(e) {
+    	e.preventDefault();
+    	$.scrollTo( this.hash, 600 );
+	});
+	$(".navbar-brand").click( function(e) {
+    	e.preventDefault();
+    	$.scrollTo( this.hash, 800, { easing:'elasout' } );
+	});
 });
